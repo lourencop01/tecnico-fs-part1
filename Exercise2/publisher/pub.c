533f75c52b95c871cd9960ab3ebf522c1a7b0e85
Lourenco Pacheco <lourencopacheco2001@gmail.com>
2023-01-05 18:34:08 +0000

    starting...

--- Exercise2/publisher/pub.c
+++ Exercise2/publisher/pub.c
@@ -1,8 +1,25 @@
 #include "logging.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(int argc, char **argv) {
     (void)argc;
-    (void)argv;
+
+    int code = -1;
+    
+    char pipe_name[256];
+    memset(pipe_name, 0, 256);
+
+    char box_name[32];
+    memset(box_name, 0, 32);
+
+    // Registo de um publisher:
+    // [ code = 1 (uint8_t) ] | [ client_named_pipe_path (char[256]) ] | [ box_name (char[32]) ]
+    sscanf(argv[1], "[ code = %d ] | [ %s ] | [ %s ]", &code, pipe_name, box_name);
+    printf("%d, %s, %s\n", code, pipe_name, box_name);
+
     fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
     WARN("unimplemented"); // TODO: implement
     return -1;
