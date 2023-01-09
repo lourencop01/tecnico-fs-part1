1715ede4b90a2a8a5b936c8d376542725023989c
Lourenco Pacheco <lourencopacheco2001@gmail.com>
2023-01-09 12:36:00 +0000

    commented manager and publisher

--- Exercise2/manager/manager.c
+++ Exercise2/manager/manager.c
@@ -12,8 +12,11 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-    (void)argc;
+    
+    // // Checks if the correct amount of arguments is passed.
+    ALWAYS_ASSERT((argc != 4 || argc != 5), "Number of arguments is incorrect, please try again.");
 
+    // Creates variables for later parsing of the command line arguments.
     char register_pipe_name[PIPE_NAME_SIZE];
     memset(register_pipe_name, '\0', PIPE_NAME_SIZE);
 
@@ -27,51 +30,63 @@ int main(int argc, char **argv) {
     strcpy(register_pipe_name, argv[1]);
     strcpy(pipe_name, argv[2]);
 
-    pipe_box_code_t *args = (pipe_box_code_t*)malloc(sizeof(pipe_box_code_t));
-    strcpy(args->name.pipe, pipe_name);
+    // Create a manager registration form.
+    pipe_box_code_t *reg = (pipe_box_code_t*)malloc(sizeof(pipe_box_code_t));
+    strcpy(reg->name.pipe, pipe_name);
 
+    // Create a pipe for the manager to communicate with the MBroker.
     int check_err = mkfifo(pipe_name, 0640);
     ALWAYS_ASSERT(check_err == 0, "Pipe could not be created.");
 
-    //Check if pipe_name is a valid path name. TODO
+    // Check if pipe_name is a valid path name. TODO
+    ALWAYS_ASSERT((strlen(pipe_name) < PIPE_NAME_SIZE), "Pipe name is too long, please try again.");
 
-    //Checks if the user wants the manager to list the boxes, or to create/remove a box. 
+    // Checks if the user wants the manager to list the boxes, or to create/remove a box.
+    // Parses the remaining arguments accordingly.
     if (!strcmp(argv[3], "create")) {
         strcpy(box_name, argv[4]);
-        strcpy(args->name.box, box_name);
-        args->code = 3;
+        strcpy(reg->name.box, box_name);
+        reg->code = 3;
 
     } else if (!strcmp(argv[3], "remove")) {
         strcpy(box_name, argv[4]);
-        strcpy(args->name.box, box_name);
-        args->code = 5;
+        strcpy(reg->name.box, box_name);
+        reg->code = 5;
 
     } else if (!strcmp(argv[3], "list")) {
-        args->code = 7;
-        strcpy(args->name.box, box_name);
+        reg->code = 7;
+        strcpy(reg->name.box, box_name);
 
     } else {
         fprintf(stderr, "Please insert a correct input command.\n");
         exit(EXIT_SUCCESS);
     }
 
+    // Opens the register pipe and writes the registration form to it.
     int register_fd = open(register_pipe_name, O_WRONLY);
     ALWAYS_ASSERT(register_fd != -1, "Could not open the register pipe.");
 
-    ssize_t bytes = write(register_fd, args, sizeof(pipe_box_code_t));
+    ssize_t bytes = write(register_fd, reg, sizeof(pipe_box_code_t));
     ALWAYS_ASSERT(bytes > 0, "Could not write to the register pipe.");
 
-    close(register_fd);
+    // Closes the register pipe.
+    ALWAYS_ASSERT(close(register_fd) == 0, "Could not close the register pipe.");
 
+    // Opens the pipe in read mode for the manager to communicate with the MBroker.
     int pipe_fd = open(pipe_name, O_RDONLY);
     ALWAYS_ASSERT(pipe_fd != -1, "Manager could not open %s.", pipe_name);
 
-    if (args->code == 3 || args->code == 5) {
+    // Checks if the manager is supposed to list the boxes, or create/remove a box.
+    if (reg->code == 3 || reg->code == 5) {
 
+        // Creates a box creation/removal form to read the reply from the MBroker.
         req_reply_t *reply = (req_reply_t*)malloc(sizeof(req_reply_t));
+        
+        // Reads the reply.
         bytes = read(pipe_fd, reply, sizeof(req_reply_t));
         ALWAYS_ASSERT(bytes == sizeof(req_reply_t), "Manager failed to read from %s.", pipe_name);
 
+        // Checks if the box was created/removed successfully.
         if (reply->ret == 0) {
             fprintf(stdout, "OK\n");
         } else {
@@ -80,18 +95,25 @@ int main(int argc, char **argv) {
 
     } else {
 
+        // Creates a box listing form to read the reply from the MBroker.
         box_listing_t *reply = (box_listing_t*)malloc(sizeof(box_listing_t));
-        bytes = read(pipe_fd, reply, sizeof(box_listing_t));
-        //printf("bytes read = %zd, sizeof is %zd\n", bytes, sizeof(box_listing_t));
-        //ALWAYS_ASSERT(bytes == sizeof(box_listing_t), "Manager failed to read from %s.", pipe_name);
 
+        // Reads the reply.
+        bytes = read(pipe_fd, reply, sizeof(box_listing_t));
+        ALWAYS_ASSERT(bytes == sizeof(box_listing_t), "Manager failed to read from %s.", pipe_name);
+
+        // Sets the number of boxes to an int variable.
         int box_number = reply->box_amount;
 
+        // Checks if there are no boxes to list.
         if (box_number == 0) {
             fprintf(stdout, "NO BOXES FOUND\n");
         } else {
-            box_t *temp_box = (box_t*)malloc(sizeof(box_t));
 
+            // Creates a temporary box to store values.
+            box_t *temp_box = (box_t*)malloc(sizeof(box_t));
+            
+            // Organizes the box list alphabetically.
             for (int i = 0; i < box_number; i++) {
                 for (int j = i + 1; j < box_number; j++) {
                     if (strcmp(reply->boxes[i].box_name,reply->boxes[j].box_name) > 0) {
@@ -115,19 +137,22 @@ int main(int argc, char **argv) {
                 }
             }
 
+            // Frees the memory allocated for the temporary box.
             free(temp_box);
 
+            // Prints the box list.
             for (int i = 0; i < box_number; i++) {
                 fprintf(stdout, "%s %zu %zu %zu\n", reply->boxes[i].box_name, 
                                             reply->boxes[i].box_size, reply->boxes[i].n_publishers, 
                                                                     reply->boxes[i].n_subscribers);
             }
-    }
+        }
     }
 
-    close(pipe_fd);
-
+    // Closes and deletes the client's pipe.
+    ALWAYS_ASSERT(close(pipe_fd) == 0, "Could not close %s.", pipe_name);
     ALWAYS_ASSERT((unlink(pipe_name) == 0), "Could not delete %s.", pipe_name);
 
+    // Exits the program.
     return -1;
 }
--- Exercise2/publisher/pub.c
+++ Exercise2/publisher/pub.c
@@ -11,26 +11,40 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/**
+ * This function is used to send messages by the publisher to
+ * its designed box.
+ * 
+ * Input:
+ *  - fd: The file descriptor of the publisher's pipe.
+*/
 void send_message(int fd) {
 
+    // Creates a buffer to store the message.
     char message[MESSAGE_SIZE];
+
+    // Creates a variable to store the number of bytes written.
     ssize_t bytes = -1;
 
-    printf("You can now start typing your messages!\n");
+    printf("Insert nuclear codes:\n");
 
+    // While loop to read the messages from stdin until CTRL+D (EOF) is pressed.
     while (fgets(message, MESSAGE_SIZE, stdin) != NULL) {
         bytes = write(fd, message, strlen(message) + 1);
         ALWAYS_ASSERT(bytes == (strlen(message) + 1), "Number of bytes written is not equal to"
                                                                     " the number of bytes read.");
     }
 
-    close(fd);
+    // Close the publisher's pipe.
+    ALWAYS_ASSERT(close(fd) == 0, "Could not close the publisher's pipe.");
 }
 
 int main(int argc, char **argv) {
 
-    (void)argc;
+    // Checks if the correct amount of arguments is passed.
+    ALWAYS_ASSERT(argc == 4, "This process takes 4 arguments, please try again.");
 
+    // Creates variables for later parsing of the command line arguments.
     char register_pipe_name[PIPE_NAME_SIZE];
     memset(register_pipe_name, '\0', PIPE_NAME_SIZE);
 
@@ -45,35 +59,49 @@ int main(int argc, char **argv) {
     strcpy(pipe_name, argv[2]);
     strcpy(box_name, argv[3]);
 
-    //Check if box_name exists. TODO
+    //Check if box_name exists nd is valid. TODO
+    ALWAYS_ASSERT(strlen(box_name) < BOX_NAME_SIZE, "Box name is too long. Please try again");
+
+    //Check if box_name already exists in tfs. TODO CHECK IF THIS WORKS
+    ALWAYS_ASSERT(access(box_name, F_OK) == -1, "Box does not exist. Try a different name.");
 
     //Check if pipe_name is a valid path name. TODO
+    ALWAYS_ASSERT(strlen(pipe_name) < PIPE_NAME_SIZE, "Pipe name is too long. Please try again");
 
     // Check if pipe_name already exists. TODO
+    ALWAYS_ASSERT(access(pipe_name, F_OK) == -1, "Pipe already exists. Try a different name.");
 
+    // Create the client pipe.
     int check_err = mkfifo(pipe_name, 0640);
     ALWAYS_ASSERT(check_err == 0, "Pipe could not be created.");
 
+    // Create a registration form.
     pipe_box_code_t *reg = (pipe_box_code_t*)malloc(sizeof(pipe_box_code_t));
     strcpy(reg->name.box, box_name);
     strcpy(reg->name.pipe, pipe_name);
     reg->code = 1;
 
+    // Open the register pipe and write the registration form.
     int register_fd = open(register_pipe_name, O_WRONLY);
     ALWAYS_ASSERT(register_fd != -1, "Could not open the register pipe.");
 
     ssize_t bytes_written = write(register_fd, reg, sizeof(pipe_box_code_t));
     ALWAYS_ASSERT(bytes_written > 0, "Could not write to the register pipe.");
 
-    close(register_fd);
+    // Close the register pipe.
+    ALWAYS_ASSERT(close(register_fd) == 0, "Could not close the register pipe.");
 
+    // Open the publisher's pipe for writing.
     int pipe_fd = open(pipe_name, O_WRONLY);
     ALWAYS_ASSERT(pipe_fd != -1, "Could not open the publisher's pipe.");
 
+    // Send the messages.
     send_message(pipe_fd);
 
+    // Free the registration form and delete the pipe.
     free(reg);
     ALWAYS_ASSERT((unlink(pipe_name) == 0), "Could not delete %s.", pipe_name);
 
+    // Exit the program.
     return -1;
 }
